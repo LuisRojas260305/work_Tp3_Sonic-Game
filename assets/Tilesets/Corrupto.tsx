8a8c785061b539ead393a7bf5f74f17376c50561
LuisRojas260305 <luisalexanderrojasguevara@gmail.com>
2025-07-22 11:10:50 -0400

    Actualice el mapa

--- assets/Tilesets/Corrupto.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Corrupto" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="1800" columns="50">
- <image source="tileds sonic corruptosl.png" width="1651" height="1203"/>
-</tileset>
---
+++ assets/Tilesets/TileSet.tsx
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TileSet" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="50" columns="10">
+ <image source="Tiles.png" width="331" height="166"/>
+ <tile id="19">
+  <properties>
+   <property name="Colision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="Spawn" type="bool" value="true"/>
+   <property name="To" value="Sonic"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="Spawn" type="bool" value="true"/>
+   <property name="To" value="Tails"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Spawn" type="bool" value="true"/>
+   <property name="To" value="Knuckles"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="Spawn" type="bool" value="true"/>
+   <property name="To" value="Egman"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="Spawn" type="bool" value="true"/>
+   <property name="To" value="Anillos"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="Spawn" type="bool" value="true"/>
+   <property name="To" value="Basura"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="Spawn" type="bool" value="true"/>
+   <property name="To" value="Pinchos"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="Spawn" type="bool" value="true"/>
+   <property name="To" value="Enemigo"/>
+  </properties>
+ </tile>
+</tileset>
